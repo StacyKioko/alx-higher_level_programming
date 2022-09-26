d6957a36a73f021fc475e4c7213c84a3744cfa51
StacyKioko <stacy.mueke22@gmail.com>
2022-09-27 01:11:59 +0300

    Updating the overriding of __str__ magic method

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+""" Creating a class """
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """A Square class that inherits from Rectangle"""
     def __init__(self, size):
-        self.integer_validator(size, size)
-        super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ area method """
+        return self.__size ** 2
 
     def __str__(self):
-        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+        """ __str__ method """
+        return "[Square] {}/{}".format(self.__size, self.__size)
